9c0899fb54f71ca80f5706b5d1ff18f3349ccf25
Rana Emad <rana3emad@gmail.com>
2020-07-30 20:22:09 +0200

    play quizes

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -86,11 +86,11 @@ def create_app(test_config=None):
         return jsonify(response)
 
     '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
+  @TODO:
+  Create an endpoint to DELETE question using a question ID.
 
   TEST: When you click the trash icon next to a question, the question will be removed.
-  This removal will persist in the database and when you refresh the page. 
+  This removal will persist in the database and when you refresh the page.
   '''
     @app.route('/questions/<question_id>', methods=['DELETE'])
     def delete_question(question_id):
@@ -104,14 +104,14 @@ def create_app(test_config=None):
         return jsonify(response)
 
     '''
-  @TODO: 
-  Create an endpoint to POST a new question, 
-  which will require the question and answer text, 
+  @TODO:
+  Create an endpoint to POST a new question,
+  which will require the question and answer text,
   category, and difficulty score.
 
-  TEST: When you submit a question on the "Add" tab, 
+  TEST: When you submit a question on the "Add" tab,
   the form will clear and the question will appear at the end of the last page
-  of the questions list in the "List" tab.  
+  of the questions list in the "List" tab.
   '''
 
     @app.route('/questions', methods=['POST'])
@@ -126,14 +126,14 @@ def create_app(test_config=None):
         return jsonify(response)
 
     '''
-  @TODO: 
-  Create a POST endpoint to get questions based on a search term. 
-  It should return any questions for whom the search term 
-  is a substring of the question. 
+  @TODO:
+  Create a POST endpoint to get questions based on a search term.
+  It should return any questions for whom the search term
+  is a substring of the question.
 
-  TEST: Search by any phrase. The questions list will update to include 
-  only question that include that string within their question. 
-  Try using the word "title" to start. 
+  TEST: Search by any phrase. The questions list will update to include
+  only question that include that string within their question.
+  Try using the word "title" to start.
   '''
     @app.route("/questions/search", methods=['POST'])
     @cross_origin()
@@ -155,12 +155,12 @@ def create_app(test_config=None):
         return jsonify(response)
 
     '''
-  @TODO: 
-  Create a GET endpoint to get questions based on category. 
+  @TODO:
+  Create a GET endpoint to get questions based on category.
 
-  TEST: In the "List" tab / main screen, clicking on one of the 
-  categories in the left column will cause only questions of that 
-  category to be shown. 
+  TEST: In the "List" tab / main screen, clicking on one of the
+  categories in the left column will cause only questions of that
+  category to be shown.
   '''
     @app.route("/categories/<category_id>/questions")
     @cross_origin()
@@ -180,16 +180,37 @@ def create_app(test_config=None):
         return jsonify(response)
 
     '''
-  @TODO: 
-  Create a POST endpoint to get questions to play the quiz. 
-  This endpoint should take category and previous question parameters 
-  and return a random questions within the given category, 
-  if provided, and that is not one of the previous questions. 
+  @TODO:
+  Create a POST endpoint to get questions to play the quiz.
+  This endpoint should take category and previous question parameters
+  and return a random questions within the given category,
+  if provided, and that is not one of the previous questions.
 
   TEST: In the "Play" tab, after a user selects "All" or a category,
   one question at a time is displayed, the user is allowed to answer
-  and shown whether they were correct or not. 
+  and shown whether they were correct or not.
   '''
+    @app.route("/quizzes", methods=["POST"])
+    @cross_origin()
+    def get_quiz():
+        previous_questions = request.get_json()["previous_questions"]
+        quiz_category = request.get_json()["quiz_category"]
+
+        question = {}
+        if quiz_category["id"] != 0:
+            quiz_category = request.get_json()["quiz_category"]
+            question = Question.query.filter_by(category=quiz_category["id"]).filter(
+                ~Question.id.in_(previous_questions)).first()
+        else:
+            question = Question.query.filter(
+                ~Question.id.in_(previous_questions)).first()
+        if question:
+            question = question.format()
+        response = {
+            "success": True,
+            "question": question
+        }
+        return jsonify(response)
 
     '''
   @TODO: 
